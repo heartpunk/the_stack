f63e71036bcd014b9f2d6561aebe4562b4adfa93
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2011-08-13 23:23:39 -0700

    made sessions take a year to expire.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TheStack::Application.config.session_store :cookie_store, :key => '_the_stack_session'
+TheStack::Application.config.session_store :cookie_store, :key => '_the_stack_session', :expire_after => 1.year
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
